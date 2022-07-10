bdaac53b75f68f00b084d095b9c301f191151bb7
ZCJ0422 <959264868@qq.com>
2022-07-10 09:42:35 +0800

    for change a little

---
+++ termscript.R
@@ -0,0 +1,46 @@
+library(stringr)
+
+#read HumanDO.obo file and select data
+f1<-data.table::fread("./HumanDO.obo",encoding = "UTF-8",fill = T,skip = 28,header = F)
+locate <-which(!is.na(str_match(f1$V1,"\\[Term\\]"))|!is.na(str_match(f1$V1,"id: DOID"))|!is.na(str_match(f1$V1,"name: "))|!is.na(str_match(f1$V1,"synonym:")))
+f1<-f1[locate,]
+
+#extract data into a dataframe
+n = nrow(f1)
+doid <- c(1:n)
+Term <- c(NA)
+Synonym <- c(NA)
+Secondary <- c(NA)
+df <- cbind(doid,Term,Synonym,Secondary)
+df <- as.data.frame(df)
+
+i=1
+for (i in 1:n){
+  if(!is.na(str_match(f1$V1[i],"\\[Term\\]"))){
+    df$doid[i] <- f1[i+1];
+    df$Term[i] <- f1[i+2];
+    j=1
+  while (is.na(str_match(f1$V1[i+2+j],"\\[Term\\]" )) & j < 50) {
+    if(!is.na(str_match(f1$V1[i+2+j],"synonym:"))){
+      df$Synonym[i] <- f1[i+2+j];
+    }
+    else if(!is.na(str_match(f1$V1[i+2+j],"alt_id:"))){
+      df$Secondary[i] <- f1[i+2+j];
+       }
+    j=j+1;
+    }
+  }
+  i=i+1
+}
+
+#process data
+locate <-which(!is.na(str_match(df$doid,"id: DOID")))
+df <- df[locate,]
+df$doid <- gsub("id: ","",df$doid)
+df$Term <- gsub("name: ","",df$Term)
+df$Synonym <- gsub("synonym: ","",df$Synonym)
+df$Secondary <- gsub("alt_id: ","",df$Secondary)
+
+#save data
+write.csv(df, file = "./DOTERM.csv")
+
